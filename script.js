140d4fd7de4032de3250b5b57f29bf9e50f972f8
Kai Franke <cosinushh@gmail.com>
2025-03-16 10:20:25 +0100

    establish .no-transition class

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const numberVeryBottom = document.querySelector(".number-four");
 const numberTopBackground = document.querySelector(".number-three");
 const flipBtn = document.getElementById("flipBtn");
 
-// Starte mit der Zahl, die aktuell oben steht (im Beispiel 4)
 let currentNumber = parseInt(numberTop.textContent, 10);
 
 // Klick-Event für den Button
@@ -22,17 +21,15 @@ flipBtn.addEventListener("click", () => {
   // Nach Ende der Transition:
   // -> Obere Zahl aktualisieren, Flip-Klasse entfernen
   const onFlipEnd = () => {
-    topFlipCard.style.transition = "none";
+    topFlipCard.classList.add("no-transition");
     topFlipCard.classList.remove("flip");
-
     numberTop.textContent = nextNumber;
     currentNumber = nextNumber;
     numberVeryBottom.textContent = nextNumber;
   };
+  
   numberTopBackground.textContent = nextNumber;
-
   // Wenn die Flip-Animation fertig ist, setze den Text in der Front
   topFlipCard.addEventListener("transitionend", onFlipEnd);
-  topFlipCard.style.transition =
-    "transform 0.6s cubic-bezier(0.84, 0, 0.98, 0.56)";
+  topFlipCard.classList.remove("no-transition");
 });
