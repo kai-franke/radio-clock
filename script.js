f7ad2327d57c9841d1503c148bfe62c4eed30b04
Kai Franke <cosinushh@gmail.com>
2025-03-16 21:13:36 +0100

    rename classes to BEM

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const flipButton = document.getElementById("flipButton");
-const flipCardForeground = document.getElementById("flipCardForeground");
-const numberForegroundTop = document.querySelector(".number-one");
-const numberForegroundBottom = document.querySelector(".number-two");
-const numberBackgroundTop = document.querySelector(".number-three");
-const numberBackgroundBottom = document.querySelector(".number-four");
+const flipCardForeground = document.querySelector(
+  ".flip-clock__card--foreground"
+);
+const numberForegroundTop = document.querySelector(
+  ".flip-clock__digit-value--foreground.flip-clock__digit-value--upper"
+);
+const numberForegroundBottom = document.querySelector(
+  ".flip-clock__digit-value--foreground.flip-clock__digit-value--lower"
+);
+const numberBackgroundTop = document.querySelector(
+  ".flip-clock__digit-value--background.flip-clock__digit-value--upper"
+);
+const numberBackgroundBottom = document.querySelector(
+  ".flip-clock__digit-value--background.flip-clock__digit-value--lower"
+);
 
 let currentNumber = parseInt(numberForegroundTop.textContent, 10);
 
@@ -12,22 +22,18 @@ flipButton.addEventListener("click", () => {
   numberForegroundBottom.textContent = nextNumber;
   numberBackgroundTop.textContent = nextNumber;
 
-  console.log('before add flip')
   flipCardForeground.classList.add("flip"); // Flip auslösen
 
   // Nach Ende der Transition:
   const onFlipEnd = () => {
-    console.log('onFlipEnd start')
     flipCardForeground.classList.add("no-transition"); // Transition für das Enfernen der .flip class deaktivieren
     flipCardForeground.classList.remove("flip");
     numberForegroundTop.textContent = nextNumber;
     numberBackgroundBottom.textContent = nextNumber;
     currentNumber = nextNumber;
     flipCardForeground.removeEventListener("transitionend", onFlipEnd);
-    console.log('onFlipEnd end')
   };
-  
+
   flipCardForeground.classList.remove("no-transition"); // Wieso kann das nicht innerhalb von onFlipEnd geschehen?
-  console.log('before transitionend')
   flipCardForeground.addEventListener("transitionend", onFlipEnd);
 });
